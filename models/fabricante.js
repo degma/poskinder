37edec0dd9306d69a664aeb1acd2cb76972e21ef
Martin Degraf <martinplate@gmail.com>
2019-05-24 00:33:08 -0300

    modicacion al modelo Fabricante

--- models/fabricante.js
+++ models/fabricante.js
@@ -6,8 +6,17 @@ module.exports = (sequelize, type) => {
           autoIncrement: true
         },
         nombre: type.STRING,
-        telefono: type.STRING,
+        markup: type.INTEGER,
+        descripcion: type.STRING,
         nombre_contacto: type.STRING,
+        apellido_contacto: type.STRING,
+        email_contacto: type.STRING,
+        telefono_contacto: type.STRING,
+        celular_contacto: type.STRING,
+        website: type.STRING,
+        calle: type.STRING,
+        localidad: type.STRING,
+        codigo_postal: type.STRING,
         notas: type.TEXT,
         activo: {
           type: type.BOOLEAN,
